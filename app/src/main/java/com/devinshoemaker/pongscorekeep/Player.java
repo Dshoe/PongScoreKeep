bb3129fdd58a0276906ce9c3c1973e59f011d1c4
Devin Shoemaker <devinshoe@gmail.com>
2016-02-08 06:42:52 -0600

    Create initial Player class

---
+++ app/src/main/java/com/devinshoemaker/pongscorekeep/Player.java
@@ -0,0 +1,18 @@
+package com.devinshoemaker.pongscorekeep;
+
+/**
+ * Created by Devin Shoemaker on 1/15/16.
+ *
+ * Used to managed each player and their score
+ */
+public class Player {
+    private int score = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
